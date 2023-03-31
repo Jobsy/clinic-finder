2f0ee287455d18b3dfd88ffa6b1f099cd0ab45d1
Oluwajoba Bello <oluwajobabello@gmail.com>
2023-03-31 07:47:09 +0100

    add readme file, linter and ci/cd

--- __tests__/searchClinic.test.js
+++ __tests__/searchClinic.test.js
@@ -1,49 +1,49 @@
-
+/* eslint-disable no-undef */
 const searchClinics = require('../services/searchClinics');
 const clinicData = require('../data/clinicData');
 
 let clinics;
 
 beforeAll(() => {
-    clinics = clinicData;
+  clinics = clinicData;
 });
 
 describe('searchClinics', () => {
-    test('should return all clinics if no parameters are provided', (done) => {
-        // wait for clinics to be fetched
-        setTimeout(() => {
-            expect(searchClinics({}, clinics)).toEqual(clinics);
-            done();
-        }, 5000); // wait for 5 seconds before checking the result
-    });
+  test('should return all clinics if no parameters are provided', (done) => {
+    // wait for clinics to be fetched
+    setTimeout(() => {
+      expect(searchClinics({}, clinics)).toEqual(clinics);
+      done();
+    }, 5000); // wait for 5 seconds before checking the result
+  });
 });
 
 describe('searchClinics', () => {
   test('returns clinics with matching name', () => {
-        const result = searchClinics({ name: 'Medical' }, clinics);
-        const expected = clinicData.filter(clinic => {
-            let clinicName = clinic.name || clinic.clinicName 
-            return /medical/i.test(clinicName)
-        });
+    const result = searchClinics({ name: 'Medical' }, clinics);
+    const expected = clinicData.filter(clinic => {
+      const clinicName = clinic.name || clinic.clinicName;
+      return /medical/i.test(clinicName);
+    });
 
-        expect(result).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 
   test('returns clinics in the provided state', () => {
-        const result = searchClinics({ state: 'CA' }, clinics);
-        const expected = clinicData.filter(clinic => {
-            let clinicState = clinic.stateName || clinic.stateCode 
-            return clinicState === 'CA'
-        });
+    const result = searchClinics({ state: 'CA' }, clinics);
+    const expected = clinicData.filter(clinic => {
+      const clinicState = clinic.stateName || clinic.stateCode;
+      return clinicState === 'CA';
+    });
 
-        expect(result).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 
   test('returns clinics with availability on the provided date', () => {
     const result = searchClinics({ availability: '2022-04-10' }, clinics);
     const expected = clinics.filter(clinic => {
       const { from, to } = clinic.availability || clinic.opening || {};
-      return from && to && from <= '2022-04-10' && '2022-04-10' <= to;
+      return from && to && from <= '2022-04-10' && to >= '2022-04-10';
     });
 
     expect(result).toEqual(expected);
@@ -52,20 +52,20 @@ describe('searchClinics', () => {
   test('returns clinics with matching name and state', () => {
     const result = searchClinics({ name: 'Medical', state: 'CA' }, clinics);
     const expected = clinics.filter(clinic => {
-        let clinicName = clinic.name || clinic.clinicName
-        let clinicState = clinic.stateName || clinic.stateCode  
-        return /medical/i.test(clinicName) && clinicState === 'CA'
+      const clinicName = clinic.name || clinic.clinicName;
+      const clinicState = clinic.stateName || clinic.stateCode;
+      return /medical/i.test(clinicName) && clinicState === 'CA';
     });
-    
+
     expect(result).toEqual(expected);
   });
 
   test('returns clinics with matching name and availability', () => {
     const result = searchClinics({ name: 'Medical', availability: '2022-04-10' }, clinics);
     const expected = clinics.filter(clinic => {
-        let clinicName = clinic.name || clinic.clinicName
-        const { from, to } = clinic.availability || clinic.opening || {};
-        return /medical/i.test(clinicName) && from && to && from <= '2022-04-10' && '2022-04-10' <= to
+      const clinicName = clinic.name || clinic.clinicName;
+      const { from, to } = clinic.availability || clinic.opening || {};
+      return /medical/i.test(clinicName) && from && to && from <= '2022-04-10' && to >= '2022-04-10';
     });
 
     expect(result).toEqual(expected);
@@ -74,9 +74,9 @@ describe('searchClinics', () => {
   test('returns clinics in the provided state and availability', () => {
     const result = searchClinics({ state: 'CA', availability: '2022-04-10' }, clinics);
     const expected = clinics.filter(clinic => {
-        let clinicState = clinic.stateName || clinic.stateCode 
-        const { from, to } = clinic.availability || clinic.opening || {};
-        return clinicState === 'CA' && from && to && from <= '2022-04-10' && '2022-04-10' <= to;
+      const clinicState = clinic.stateName || clinic.stateCode;
+      const { from, to } = clinic.availability || clinic.opening || {};
+      return clinicState === 'CA' && from && to && from <= '2022-04-10' && to >= '2022-04-10';
     });
 
     expect(result).toEqual(expected);
--- data/clinicData.js
+++ data/clinicData.js
@@ -1,33 +1,39 @@
 
 const https = require('https');
+const debug = require('debug')('fetch-clinics');
 
 const fetchClinics = () => {
   const clinics = [];
 
+  // Define an array of URLs to fetch the data from
   const urls = [
     'https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json',
     'https://storage.googleapis.com/scratchpay-code-challenge/vet-clinics.json'
   ];
 
+  // Loop through each URL and make an HTTPS GET request to fetch the data
   urls.forEach((url) => {
     https.get(url, (res) => {
       let data = '';
 
+      // Listen for 'data' events and append the data to the 'data' variable
       res.on('data', (chunk) => {
         data += chunk;
       });
 
+      // Listen for the 'end' event, parse the data and add it to the clinics array
       res.on('end', () => {
         clinics.push(...JSON.parse(data));
-        console.log(`Fetched clinics from ${url}`);
+        debug(`Fetched clinics from ${url}`);
       });
     }).on('error', (err) => {
-      console.log(`Error fetching clinics from ${url}: ${err.message}`);
+      // Listen for 'error' events and log any errors that occur
+      debug(`Error fetching clinics from ${url}: ${err.message}`);
     });
   });
 
+  // Return the clinics array, which may be empty if the HTTPS requests are still in progress
   return clinics;
 };
 
 module.exports = fetchClinics();
-
