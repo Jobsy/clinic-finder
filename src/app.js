b19b0920076024b195d036d5fe92aed5a9135fa4
Oluwajoba Bello <oluwajobabello@gmail.com>
2023-04-03 23:34:34 +0100

    add netlify and tidy up code

--- src/app.js
+++ src/app.js
@@ -5,5 +5,8 @@ const clinicRoutes = require('./routes/clinicRoutes');
 const app = express();
 
 app.use('/clinics', clinicRoutes);
+app.use(`/.netlify/functions/app`, clinicRoutes);
 
 module.exports = app;
+module.exports.handler = serverless(app);
+
