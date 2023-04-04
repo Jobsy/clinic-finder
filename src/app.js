50af8ac7307c223ec586cc01ba6d86f712261bea
Oluwajoba Bello <oluwajobabello@gmail.com>
2023-04-04 03:16:37 +0100

    rollback

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 
 const express = require('express');
-const serverless = require("serverless-http");
 const clinicRoutes = require('./routes/clinicRoutes');
 
 const app = express();
 
 app.use('/clinics', clinicRoutes);
-app.use(`/.netlify/functions/app`, clinicRoutes);
 
 module.exports = app;
-module.exports.handler = serverless(app);
 
